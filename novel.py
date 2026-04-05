5be61e36b5bff026a67296fffc8c4d975863b07d
赵庆举 <2723734135@qq.com>
2026-04-05 12:00:37 +0800

    解决网址寻址和总章节数的问题，优化参数的输入方式

--- novel.py
+++ novel.py
@@ -1,8 +1,23 @@
-import traceback
-
 import requests as me
 import bs4 as i
 import re
+from urllib.parse import urlparse
+
+# TODO 只需求改URL
+url = "https://www.biquge.tw/book/1/1.html"
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/138.0.0.0 Safari/537.36",
+    "Cookie": "_ga=GA1.1.513633547.1774977737; _im_vid=01KN6SN934WXAXEJ78MDS3CY6X; "
+              "oid=%257B%2522oid%2522%253A%25227d368d35-2e79-11f1-9d22-42010a00004b%2522%252C%2522ts%2522%253A"
+              "-62135596800%252C%2522v%2522%253A%252220201117%2522%257D; "
+              "_ga_6FXZK5TW8D=GS2.1.s1775159485$o3$g1$t1775159498$j47$l0$h0; "
+              "cf_clearance=owGmC7fBRQ7RR3f75zdiNX.48QTZ583g0rmmieLcXlc-1775161096-1.2.1.1"
+              "-nf6Ow9iUFzvFESD7jVWxmsBwWXV7yyEZw1ZebyxzN6gwfSS8NEcEnzET_XqAJmhguDGcIgedPQUdHMkgvpGqdm.rIZuk"
+              ".xVWfKvveku6ouVhtq7HarKcBnWQzImyhwnduAxkG6orNcI"
+              ".HVeG8YM3nfnKgAtgeI57BTIOG8qZPtyO99BJwnTSpDLyjx5REeGf9Ljyq7e5yngm3O68IYHN9SfuI0wFxb2sbDrzfIl4Go0 "
+}
+host = ""
 
 
 def writeFile(index, name, data):
@@ -15,7 +30,7 @@ def writeFile(index, name, data):
         file.write('<head><title>')
         file.write(f'{name}')
         file.write('</title></head><body>')
-        file.write(f"<h1>{name}</h1>")
+        file.write(f"<h1>{index:03 }{name}</h1>")
         file.write(data)
         file.write('</body></html>')
 
@@ -30,6 +45,10 @@ def haveNextPage(page):
 
 def nextPageLink(page):
     aLable = page.find('a', string='下一页')
+    if aLable:
+        pass
+    else:
+        aLable = page.find('a', string='下一章')
     if aLable and 'href' in aLable.attrs:
         return aLable['href']
     else:
@@ -54,61 +73,37 @@ def getContextAndPage(reponse):
     return result.strip(), page
 
 
-def di(header, host, page):
-    print(host + nextPageLink(page))
-    reponse = me.get(host + nextPageLink(page), headers=header, timeout=(3, 5))
-    result, page = getContextAndPage(reponse)
-    if haveNextPage(page):
-        result += di(header, host, page)
-    return result
-
-
-def pa(bookid, index, offset, header, host, router):
-    offset -= 1
-    url = host + router + f"/{bookid}/" + str(index + offset) + ".html"
+def pa(url):
+    if url.startswith("https", 0, 5):
+        pass
+    else:
+        url = host + url
     print(url)
-    reponse = me.get(url, headers=header, timeout=(3, 5))
-    print(reponse.status_code)
+    reponse = me.get(url, headers=header, timeout=(1, 2))
     result, page = getContextAndPage(reponse)
+    url = nextPageLink(page)
     if haveNextPage(page):
-        result += di(header, host, page)
-    return getBookName(reponse), result
+        t1, url, abandon = pa(url)
+        result += t1
+    return result, url, getBookName(reponse)
 
 
-def run(bookid, index, offset, header, host, router):
-    temp = offset
-    # TODO 在这里输入需要下载到第end章，目前版本需要在网页中查看目录确定总章数。请务必输入正确的参数，如果end数值小于实际章数，会导致下载不完整；如果end
-    #  数值大于实际参数，会导致程序下载完后不断进行失败尝试死循环，此时使用Control+C中断程序不会影响已保存的结果
-    end = 1
+# TODO 如果你想从中间某章节开始下载，则应修改start参数为对应章节数，避免下载结果的顺序发生错乱
+# TODO 如果你想下载中间某部分章节而不是想下载整本书，则应该修改end参数为结束章节数；同时修改select为2
+def run(url, start=1, end=1, select=1):
     try:
-        for i in range(offset, end + 1):
-            name, result = pa(bookid, index, i, header, host, router)
-            writeFile(i, name, result)
-            temp += 1
+        while True and select == 1 or start <= end and select == 2:
+            result, url, name = pa(url)
+            writeFile(start, name, result)
+            if len(url) < 20:
+                break
+            start += 1
     except BaseException:
         print("错误重试")
-        traceback.print_exc()
-        # run(bookid, index, temp, header, host, router)
+        run(url, start, end)
 
 
 if __name__ == '__main__':
-    header = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/138.0.0.0 Safari/537.36",
-        "Cookie": "_ga=GA1.1.513633547.1774977737; _im_vid=01KN6SN934WXAXEJ78MDS3CY6X; "
-                  "oid=%257B%2522oid%2522%253A%25227d368d35-2e79-11f1-9d22-42010a00004b%2522%252C%2522ts%2522%253A"
-                  "-62135596800%252C%2522v%2522%253A%252220201117%2522%257D; "
-                  "_ga_6FXZK5TW8D=GS2.1.s1775159485$o3$g1$t1775159498$j47$l0$h0; "
-                  "cf_clearance=owGmC7fBRQ7RR3f75zdiNX.48QTZ583g0rmmieLcXlc-1775161096-1.2.1.1"
-                  "-nf6Ow9iUFzvFESD7jVWxmsBwWXV7yyEZw1ZebyxzN6gwfSS8NEcEnzET_XqAJmhguDGcIgedPQUdHMkgvpGqdm.rIZuk"
-                  ".xVWfKvveku6ouVhtq7HarKcBnWQzImyhwnduAxkG6orNcI"
-                  ".HVeG8YM3nfnKgAtgeI57BTIOG8qZPtyO99BJwnTSpDLyjx5REeGf9Ljyq7e5yngm3O68IYHN9SfuI0wFxb2sbDrzfIl4Go0 "
-    }
-    host = "https://biquge.tw"
-    router = "/book"
-    # TODO 通过URL获取到这两个参数，详细过程参照README.md
-    bookid = 9002
-    index = 286409
-    # 默认从第一章开始下载，如果你想要下载某些章节，请求改此参数为起始章节
-    offset = 1
-    run(bookid, index, offset, header, host, router)
+    urlObj = urlparse(url)
+    host = f"{urlObj.scheme}://{urlObj.hostname}"
+    run(url)
